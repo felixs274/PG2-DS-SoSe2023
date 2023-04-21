e60819de7ee90d2046e5d93ee210a9dd51fd2717
simon.wagner <simon_wagner1@gmx.net>
2023-04-21 17:02:58 +0200

    DS 3.2: fixes

--- DS/Uebung03/DS-1-3_U3_2.s
+++ DS/Uebung03/DS-1-3_U3_2.s
@@ -1,6 +1,6 @@
 .section .rodata
 outstring:
-    .asciz " 'A' - 0454 + 0x12345678 = 0x%X\n"
+    .asciz " 'A' - 0454 + 0x12345678 = 0x%lX\n"
 
 .section .text
 .globl main
@@ -9,12 +9,16 @@ outstring:
 main:
     pushq %rbp
 
-    xor %rsi, %rsi # clear register so we dont have leftover data
+    xor %esi, %esi # clear register so we don't have leftover data.
     movb $'A', %sil # 8 bit size
-    subw $454, %si # 16 bit size
+    subw $0454, %si # 16 bit size
     addl $0x12345678, %esi # 32 bit size
+    movslq %esi, %rsi
+
     mov $outstring, %rdi
     call printf
 
-    mov $0, %rdi
+    mov $0, %rdi # return 0 as return value. We didn't xor to be more descriptive
     call exit
+
+    pop %rbp

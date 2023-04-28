771c8d374918370d5d3815c20a18b0a0afce6a64
simon.wagner <simon_wagner1@gmx.net>
2023-04-28 15:45:58 +0200

    DS: Uebung 4.1 bearbeitet

--- DS/Uebung04/DS-1-3_U4_1.s
+++ DS/Uebung04/DS-1-3_U4_1.s
@@ -1,9 +1,46 @@
-.section .text
-.globl _start
+.section .data
+decstr:
+    .asciz "%ld\n"
 
-_start:
-    movq $50000000000,  %rax
-    movl $50000000,     %eax
-    movw $50000,        %ax
-    movb $50,           %al
-    movb $5,            %ah
+hexstr:
+    .asciz "0x%lX\n"
+
+.section .text
+
+.globl main
+.type main, @function
+
+main:
+
+    pushq %rbp
+
+    movq $50000000000, %rax
+
+    # work around
+    # create bitmask for upper bytes
+    # remove lower 32 bit from rax
+    # write new eax value with or
+    movq  $1, %rbx
+    salq $32, %rbx
+    subq $1, %rbx
+    salq $32, %rbx
+    andq %rbx, %rax
+    orq     $50000000, %rax
+
+    movw       $50000, %ax
+    movb          $50, %al
+    movb           $5, %ah
+
+    mov %rax, %rbx # save value since rax can be overwritten
+
+    mov $decstr, %rdi
+    mov %rbx, %rsi
+    call printf
+
+    mov $hexstr , %rdi
+    mov %rbx, %rsi
+    call printf 
+
+    mov $0, %rax
+    pop %rbp
+    ret

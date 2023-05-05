4d503b0881e8a6a2ce7164eb2f4b677cfc013010
felixs274 <felix.scholzen27@gmail.com>
2023-05-05 15:15:07 +0200

    PG2 5.2 bearbeitet

---
+++ PG2/Uebung05/person.h
@@ -0,0 +1,18 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <iostream>
+#include <string>
+
+enum class EyeColor {Blue, Brown, Green};
+
+struct PStruct{
+    std::string s_; // surname
+    EyeColor e_;
+    double h_;
+    bool g_; // gender 0 = male, 1 = female
+    std::ostream & print(std::ostream &);
+};
+
+std::ostream & operator <<(std::ostream &, PStruct &);
+
+#endif // PERSON_H

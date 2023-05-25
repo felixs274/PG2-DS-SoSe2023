e3b01261f24f621c45c57a2e511b83eda5406ea8
simon.wagner <simon_wagner1@gmx.net>
2023-05-25 18:35:04 +0200

    PG2: Uebung 06 kleine überarbeitungen

--- PG2/Uebung06/PG2-3-3_U6_1_midpoint.cpp
+++ PG2/Uebung06/PG2-3-3_U6_1_midpoint.cpp
@@ -1,41 +1,37 @@
 #include "midpoint.h"
 #include <cmath>
 
-
-
 Point::Point(double x, double y, double z) : x_(x), y_(y), z_(z) {}
 Point::Point(double x, double y) : Point(x, y, 0.0) {}
 Point::Point(double x) : Point(x, 0.0, 0.0) {}
 
-std::ostream & operator <<(std::ostream & out, Point & point) {
+std::ostream &operator<<(std::ostream &out, Point &point)
+{
     out << "(" << point.x_ << ", " << point.y_ << ", " << point.z_ << ")";
     return out;
 }
 
-
-
-
 Midpoint::Midpoint(Point p1, Point p2) : p1_(p1), p2_(p2) {}
 
-Point Midpoint::getMidpoint() {
+Point Midpoint::getMidpoint()
+{
     double x = (p1_.x_ + p2_.x_) / 2.0;
     double y = (p1_.y_ + p2_.y_) / 2.0;
     double z = (p1_.z_ + p2_.z_) / 2.0;
     return Point(x, y, z);
 }
 
-std::ostream & operator <<(std::ostream & out, Midpoint & midpoint) {
+std::ostream &operator<<(std::ostream &out, Midpoint &midpoint)
+{
     Point center = midpoint.getMidpoint();
-    out << "(" << center.x_ << ", " << center.y_ << ", " << center.z_ << ")";
+    out << center;
     return out;
 }
 
-
-
-
 Distance::Distance(Midpoint m1, Midpoint m2) : m1_(m1), m2_(m2) {}
 
-double Distance::getDistance() {
+double Distance::getDistance()
+{
     Point midpoint1 = m1_.getMidpoint();
     Point midpoint2 = m2_.getMidpoint();
     double x = std::pow((midpoint1.x_ - midpoint2.x_), 2);
@@ -45,7 +41,8 @@ double Distance::getDistance() {
     return dist;
 }
 
-std::ostream & operator <<(std::ostream & out, Distance & dist) {
+std::ostream &operator<<(std::ostream &out, Distance &dist)
+{
     out << dist.getDistance();
     return out;
 }

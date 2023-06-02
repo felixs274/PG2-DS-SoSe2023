454ae051e7e22695118910163dabbda30e2ea52a
simon.wagner <simon_wagner1@gmx.net>
2023-06-02 11:05:47 +0200

    PG 7.1: bearbeitet

--- PG2/Uebung07/PG2-3-3_U7_1_main.cpp
+++ PG2/Uebung07/PG2-3-3_U7_1_main.cpp
@@ -5,27 +5,36 @@ using namespace std;
 
 class A
 {
-    public:
-    A(double arg1, ostream& out)
+public:
+    A(double val, ostream& out)
+        : m_val{val}
     {
         m_initCount++;
-        out << m_initCount << ". object: " << arg1 << endl;
+        out << m_initCount << ". object: ";
+    }
+
+    operator double()
+    {
+        return m_val;
     }
 
     void static statistics(ostream& out)
     {
         out << "A total of " << m_initCount << " objects was created." << endl;
     }
-    private:
+
+private:
+    double m_val;
     static int m_initCount;
 };
+
 int A::m_initCount = 0;
 
 int main()
 {
-    A a(12.34, cout);
-    A b(-34.56, cout);
-    A c(99.88, cout);
+    A a(12.34, cout);       cout << a << endl;
+    A b(-34.56, cout);      cout << b << endl;
+    A c(99.88, cout);       cout << c << endl;
     A::statistics(cout);
     return 0;
 }

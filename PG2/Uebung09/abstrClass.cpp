4c60c6d632acd0990602f5136fc9d226949cad1a
Simon <simon_wagner1@gmx.net>
2023-06-21 22:13:59 +0200

    PG 9: abstractClass implementiert

--- PG2/Uebung09/abstrClass.cpp
+++ PG2/Uebung09/abstrClass.cpp
@@ -5,36 +5,38 @@ Figure::Figure(const P &center) : center_(center){};
 
 Figure::~Figure(){};
 
-void Figure::print2(std::ostream &out) const {
-    out << "(" << center_.x_ << ", " << center_.y_ << ")";
+void Figure::print2(std::ostream &out) const
+{
+    out << "Figure::print2" << std::endl;
 }
 
-std::ostream & operator << (std::ostream &out, const Figure &fig) {
+std::ostream &operator<<(std::ostream &out, const Figure &fig)
+{
     return fig.print1(out);
 }
 
 // Line
 Line::Line(const P &center) : Figure(center){};
 
-std::ostream & Line::print1(std::ostream &out) const {
-    out << "Center Line: ";
-    print2(out);
+std::ostream &Line::print1(std::ostream &out) const
+{
+    out << "Center Line: (" << center_.x_ << ", " << center_.y_ << ")" << std::endl;
     return out;
 }
 
 // Ellipse
 Ellipse::Ellipse(const P &center) : Figure(center){};
 
-std::ostream & Ellipse::print1(std::ostream &out) const {
-    out << "Center Ellipse: ";
-    print2(out);
+std::ostream &Ellipse::print1(std::ostream &out) const
+{
+    out << "Center Ellipse: (" << center_.x_ << ", " << center_.y_ << ")" << std::endl;
     return out;
 }
 
 // Circle
 Circle::Circle(const P &center) : Ellipse(center) {}
 
-void Circle::print2(std::ostream &out) const {
-    Ellipse::print2(out);
-    out << " Center Circle: ";
+void Circle::print2(std::ostream &out) const
+{
+    out << "Center Circle: (" << center_.x_ << ", " << center_.y_ << ")" << std::endl;
 }

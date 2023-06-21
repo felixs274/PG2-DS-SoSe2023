6641dda30df925b890e487fad6bc477620247f03
Simon <simon_wagner1@gmx.net>
2023-06-21 22:29:44 +0200

    PG9) main fixed

--- PG2/Uebung09/main.cpp
+++ PG2/Uebung09/main.cpp
@@ -11,8 +11,8 @@ int main()
     pF[1] = new Ellipse(P{2.1, 2.2});
     pF[2] = new Circle(P{3.1, 3.2});
 
-    cout << pF[0];
-    cout << pF[1];
+    cout << *pF[0];
+    cout << *pF[1];
     pF[2]->print2(cout);
     return 0;
 }

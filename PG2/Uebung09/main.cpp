c330043118b7703f2a4e26eaaaa386d276260e67
simon.wagner <simon_wagner1@gmx.net>
2023-06-21 19:04:53 +0200

    PG 9: fixed build

--- PG2/Uebung09/main.cpp
+++ PG2/Uebung09/main.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    Figure * pF[3];
+    Figure *pF[3];
 
-    pF[0] = new Line(P(1.1, 1.2));
-    pF[1] = new Ellipse(P(2.1, 2.2));
-    pF[2] = new Circle(P(3.1, 3.2));
+    pF[0] = new Line({1.1, 1.2});
+    pF[1] = new Ellipse({2.1, 2.2});
+    pF[2] = new Circle({3.1, 3.2});
 
-    for (auto item : pF) {
+    for (auto item : pF)
+    {
         cout << *item;
     }
 
-    for (auto item: pF) {
-        item -> print2(cout);
+    for (auto item : pF)
+    {
+        item->print2(cout);
     }
 
     return 0;
